126e962a07484bdfa2bb79b1cbc84815478f3de5
Jedo0224 <qkrwotjr0@gmail.com>
2024-03-05 15:01:36 +0900

    [feat] 위경도를 스캔하기 위한 권한 부여 추가, [chore] 모니터링 및 도면 설정 이동시 라우팅 경로 수정

--- app/src/main/java/com/example/asan_service/MainActivity.kt
+++ app/src/main/java/com/example/asan_service/MainActivity.kt
@@ -1,44 +1,90 @@
 package com.example.asan_service
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.asan_service.core.AppDatabase
+import com.example.asan_service.dao.WatchItemDao
+import com.example.asan_service.feature.MoniteringScreen
 import com.example.asan_service.feature.StatisticScreen
 import com.example.asan_service.feature.WatchSettingScreen
 import com.example.asan_service.ui.theme.Asan_ServiceTheme
 import com.example.asan_service.viewmodel.ConnectScreenViewModel
+import com.example.asan_service.viewmodel.ImageViewModel
+import com.example.asan_service.viewmodel.MonitorViewModel
 import com.example.asan_service.viewmodel.StaticalViewModel
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var db: AppDatabase
-
+    private val MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         db = AppDatabase.getInstance(applicationContext)
         startService(Intent(this, MyWebSocketService::class.java))
 
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한이 승인되지 않았다면 요청합니다
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한이 승인되지 않았다면 요청합니다
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+
+
+
+
         setContent {
             val navController = rememberNavController()
+
+
             Asan_ServiceTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                     NavHost(
                         navController = navController,
                         startDestination = "MainScreen"
                     ) {
                         composable("MainScreen") {
-                            MainScreen(navController)
+                            MainScreen(navController, ImageViewModel())
                         }
                         composable("ConnectScreen") {
                             ConnectScreen(navController, ConnectScreenViewModel(db.watchItemDao()))
@@ -60,13 +106,38 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable("BackgroundSettingScreen") {
-                            BackgroundSettingScreen(navController)
+                            BackgroundSettingScreen(navController, ImageViewModel())
                         }
-                        composable("WatchSettingScreen") {
-                            WatchSettingScreen(navController, "dfdf")
+                        composable("BackgroundDetailScreen/{imageId}?imageName={imageName}") { backStackEntry ->
+                            val viewModel = ImageViewModel() // 필요에 따라 파라미터 추가
+                            val imageId = backStackEntry.arguments?.getString("imageId")
+                            BackgroundDetailScreen(navController, viewModel)
                         }
+                        composable("ScannerSettingScreen") {
+                            ScannerSettingScreen(navController,ConnectScreenViewModel(db.watchItemDao()))
+                        }
+                        composable("WatchSettingScreen/{watchId}") {
+                            WatchSettingScreen(navController,ImageViewModel())
+                        }
+                        composable("MoniteringScreen/{imageId}?imageName={imageName}") {
+                            MoniteringScreen(navController, ImageViewModel(), MonitorViewModel())
+                        }
+
                     }
                 }
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // 권한이 승인되었습니다. 위치 관련 작업을 계속할 수 있습니다.
+            } else {
+                // 권한이 거부되었습니다. 사용자에게 권한이 필요한 이유를 설명하거나
+                // 권한이 없을 때의 대체 작업을 처리하세요.
             }
         }
     }
