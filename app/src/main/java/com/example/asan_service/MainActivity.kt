3888aa4b7a4cfd8cce9281f664af79f6288771c1
Jedo0224 <qkrwotjr0@gmail.com>
2024-04-28 13:39:01 +0900

    [fix] 도면 삭제 시 이미지 목록이 업데이트되지 않던 문제 해결

--- app/src/main/java/com/example/asan_service/MainActivity.kt
+++ app/src/main/java/com/example/asan_service/MainActivity.kt
@@ -4,33 +4,25 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.asan_service.core.AppDatabase
-import com.example.asan_service.dao.WatchItemDao
 import com.example.asan_service.feature.MoniteringScreen
 import com.example.asan_service.feature.StatisticScreen
 import com.example.asan_service.feature.WatchSettingScreen
 import com.example.asan_service.ui.theme.Asan_ServiceTheme
 import com.example.asan_service.viewmodel.ConnectScreenViewModel
 import com.example.asan_service.viewmodel.ImageViewModel
-import com.example.asan_service.viewmodel.MonitorViewModel
 import com.example.asan_service.viewmodel.ScannerSettingViewModel
 import com.example.asan_service.viewmodel.StaticalViewModel
 
@@ -44,6 +36,7 @@ class MainActivity : ComponentActivity() {
         db = AppDatabase.getInstance(applicationContext)
         startService(Intent(this, MyWebSocketService::class.java))
         viewModel = ViewModelProvider(this)[ImageViewModel::class.java]
+
         viewModel.getImageList()
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -69,6 +62,9 @@ class MainActivity : ComponentActivity() {
             )
         }
 
+
+
+
         setContent {
             val navController = rememberNavController()
 
@@ -84,7 +80,7 @@ class MainActivity : ComponentActivity() {
                         startDestination = "MainScreen"
                     ) {
                         composable("MainScreen") {
-                            MainScreen(navController, viewModel)
+                            MainScreen(navController,viewModel)
                         }
                         composable("ConnectScreen") {
                             ConnectScreen(navController, ConnectScreenViewModel(db.watchItemDao()))
@@ -94,30 +90,28 @@ class MainActivity : ComponentActivity() {
                         }
                         composable("StatisticScreen") {
                             StatisticScreen(navController, StaticalViewModel(db.watchItemDao(),
-                            db.accXDao(),
-                            db.accYDao(),
-                            db.accZDao(),
-                            db.heartRateDao())
+                                db.accXDao(),
+                                db.accYDao(),
+                                db.accZDao(),
+                                db.heartRateDao())
                             )
                         }
                         composable("BackgroundSettingScreen") {
                             BackgroundSettingScreen(navController, viewModel)
                         }
                         composable("BackgroundDetailScreen/{imageId}?imageName={imageName}") { backStackEntry ->
-                            val viewModel = ImageViewModel() // 필요에 따라 파라미터 추가
-                            val imageId = backStackEntry.arguments?.getString("imageId")
                             BackgroundDetailScreen(navController, viewModel)
                         }
                         composable("ScannerSettingScreen") {
                             ScannerSettingScreen(navController,ConnectScreenViewModel(db.watchItemDao()))
                         }
                         composable("WatchSettingScreen/{watchId}") {
-                            WatchSettingScreen(navController, viewModel,
+                            WatchSettingScreen(navController,viewModel,
                                 ScannerSettingViewModel(db.watchItemDao(),db.nickNameDao())
                             )
                         }
                         composable("MoniteringScreen/{imageId}?imageName={imageName}") {
-                            MoniteringScreen(navController, viewModel)
+                            MoniteringScreen(navController, viewModel )
                         }
 
                     }
@@ -125,6 +119,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<out String>,
@@ -140,4 +136,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
