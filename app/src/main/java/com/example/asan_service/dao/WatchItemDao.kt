b4b67f9de2017c8b18fd61bbca02f0d149cd4e85
Jedo0224 <qkrwotjr0@gmail.com>
2024-05-14 22:04:39 +0900

    [feat] 워치 삭제 쿼리 추가

--- app/src/main/java/com/example/asan_service/dao/WatchItemDao.kt
+++ app/src/main/java/com/example/asan_service/dao/WatchItemDao.kt
@@ -2,7 +2,6 @@ package com.example.asan_service.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.example.asan_service.entity.NickNameEntity
 import com.example.asan_service.entity.WatchItemEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -23,8 +22,13 @@ interface WatchItemDao {
     @Delete
     fun delete(watchItem: WatchItemEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrUpdateWatchItem(watchItem: WatchItemEntity)
+
     @Query("UPDATE watch_item_entities SET patientName = :newName WHERE watchId = :watchId")
     fun updatePatientName(watchId: String, newName: String)
-    @Query("DELETE FROM watch_item_entities")
-    fun deleteAll()
+
+
+    @Query("DELETE FROM watch_item_entities WHERE watchId = :watchId")
+    fun deleteWatch(watchId: String)
 }
