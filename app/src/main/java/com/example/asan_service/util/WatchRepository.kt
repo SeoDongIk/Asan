3cd6929d671380860070d3affafa6601050788e9
Jedo0224 <qkrwotjr0@gmail.com>
2024-05-24 13:54:20 +0900

    [feat] WatchRepository를 통해 watchListLiveData를 관리.

---
+++ app/src/main/java/com/example/asan_service/util/WatchRepository.kt
@@ -0,0 +1,39 @@
+package com.example.asan_service.util
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.asan_service.ApiService
+import com.example.asan_service.dao.WatchItemDao
+import com.example.asan_service.parser.WatchItem
+
+class WatchRepository(private val userDao: WatchItemDao, private val apiService: ApiService) {
+
+    private val _watchListLiveData = MutableLiveData<List<WatchItem>>()
+    val watchListLiveData: LiveData<List<WatchItem>> = _watchListLiveData
+
+     suspend fun fetchWatchList() {
+        try {
+            val response = apiService.getWatchList("9999999")
+            if (response.status == 200) {
+                val watchList = response.data.watchList
+                Log.e("watchListwatchList", watchList.toString())
+                _watchListLiveData.postValue(watchList)
+            }
+        } catch (e: Exception) {
+            Log.e("WatchRepository", "Error fetching watch list", e)
+        }
+    }
+
+    fun deleteWatch(id: Long) {
+        try {
+            val response = apiService.deleteWatch(id).execute()
+            if (response.isSuccessful) {
+                userDao.deleteWatch(id.toString())
+                _watchListLiveData.postValue(_watchListLiveData.value?.filter { it.watchId != id })
+            }
+        } catch (e: Exception) {
+            Log.e("WatchRepository", "Error deleting watch", e)
+        }
+    }
+}
