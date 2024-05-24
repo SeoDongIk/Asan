c696ae6aab8cfb9e9a210b4c098903687d2340ae
Jedo0224 <qkrwotjr0@gmail.com>
2024-05-24 13:53:30 +0900

    [feat] severUrl 및 retrofit 연결 변수 StaticResource로 통합

---
+++ app/src/main/java/com/example/asan_service/util/StaticResource.kt
@@ -0,0 +1,26 @@
+package com.example.asan_service.util
+
+import com.example.asan_service.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object StaticResource {
+    private const val ServerURL = "210.102.178.186"
+    private const val port = "8080"
+
+    val apiService: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(getHttpURL())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+
+    fun getHttpURL(): String {
+        return if (port.isEmpty()) "http://$ServerURL/" else "http://$ServerURL:$port/"
+    }
+
+    fun getWsURL(): String {
+        return if (port.isEmpty()) "ws://$ServerURL/" else "ws://$ServerURL:$port/ws"
+    }
+}
