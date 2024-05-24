22aab813bec3c073a231f6268a1459e3c1440389
Jedo0224 <qkrwotjr0@gmail.com>
2024-05-24 13:50:32 +0900

    [feat] 새로운 워치 추가시 실시간으로 반영하도록 변경, [refactor] 소켓 연결 함수 분리

--- app/src/main/java/com/example/asan_service/MyWebSocketService.kt
+++ app/src/main/java/com/example/asan_service/MyWebSocketService.kt
@@ -6,20 +6,16 @@ import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import androidx.room.Room
 import com.example.asan_service.core.ApiService
 import com.example.asan_service.core.AppDatabase
-import com.example.asan_service.dao.AccXDao
-import com.example.asan_service.dao.AccYDao
-import com.example.asan_service.dao.AccZDao
-import com.example.asan_service.dao.WatchItemDao
 import com.example.asan_service.entity.*
 import com.example.asan_service.parser.WatchItem
+import com.example.asan_service.util.PositionRepository
+import com.example.asan_service.util.StaticResource
+import com.example.asan_service.util.WatchRepository
 
-import com.example.asan_service.viewmodel.MonitorViewModel
+
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
 import okhttp3.OkHttpClient
 import org.json.JSONObject
 import ua.naiksoftware.stomp.Stomp
@@ -27,7 +23,6 @@ import ua.naiksoftware.stomp.StompClient
 import ua.naiksoftware.stomp.dto.LifecycleEvent
 import ua.naiksoftware.stomp.dto.StompHeader
 import ua.naiksoftware.stomp.dto.StompMessage
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MyWebSocketService : Service() {
@@ -36,6 +31,8 @@ class MyWebSocketService : Service() {
     private val NOTIFICATION_ID = 12345
     private lateinit var db: AppDatabase
     private lateinit var client : StompClient
+    private val watchRepository by lazy { WatchRepository(db.watchItemDao(), StaticResource.apiService) }
+    private val timers = mutableMapOf<String, Job>()
 
     override fun onCreate() {
         super.onCreate()
@@ -49,178 +46,160 @@ class MyWebSocketService : Service() {
             db.accYDao().deleteAllData()
             db.accZDao().deleteAllData()
             db.heartRateDao().deleteAllData()
-
         }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("service2", "서비스 시작")
         val watchService = ApiService.create()
-        GlobalScope.launch(Dispatchers.IO) {
+        val scope = CoroutineScope(Dispatchers.IO + Job())
+
+        scope.launch {
             try {
-                val response = watchService.getWatchList("9999999")
-                if (response.status == 200) {
-                    val watchList = response.data.watchList
-                    Log.d("watchList",watchList.toString());
+                watchRepository.fetchWatchList()
+                delay(3000)
+                var watchList = watchRepository.watchListLiveData.value?.toMutableList()
+
+                if (watchList != null) {
+                    Log.d("service2", "0-1차 연결 : $watchList")
                     val watchIds: Set<String> = watchList.map { it.watchId.toString() }.toSet()
 
-                    Log.d("service2", "0-1차 연결 : " + response.toString())
+                    client.waitForConnection()
+                    Log.d("service2", "0-2차 연결 : 소켓 연결 완료 (구독 준비 중)")
 
-                    GlobalScope.launch(Dispatchers.IO) {
-                        client.waitForConnection()
-                        Log.d("service2", "0-2차 연결 : " + "소켓 연결 완료 (구독 준비 중)")
-
-                        val deferred = CompletableDeferred<Unit>()
-
-//                        val watchItemEntities = watchList.map {
-//                            WatchItemEntity(
-//                                watchId = it.watchId.toString(),
-//                                patientName = it.name,
-//                                patientRoom = it.host,
-//                                isConnected = true,
-//                                measuredDate = System.currentTimeMillis(),
-//                                modelName = "Abcdefghd"
-//                            )
-//                        }
-//
-//
-//                        db.watchItemDao().insertAll(watchItemEntities)
-
-
-                        Log.d("service2", "1차 연결 : " + "연결된 워치 구독 시작")
-
-                        client.lifecycle().subscribe { lifecycleEvent ->
-                            when (lifecycleEvent.type) {
-                                LifecycleEvent.Type.OPENED -> {
-                                    Log.d("service2", " 1차 연결 : OPENED")
-                                }
-                                LifecycleEvent.Type.CLOSED -> {
-                                    Log.d("service2", "1차 연결 : CLOSED")
-                                }
-                                LifecycleEvent.Type.ERROR -> {
-                                    Log.d("service2", "1차 연결 : ERROR " +  lifecycleEvent.exception.toString())
-                                }
-                                else ->{
-                                    Log.d("service2", "1차 연결 : ELSE " + lifecycleEvent.message)
-                                }
-                            }
-                        }
-
-                        client.subscribe("/queue/sensor/9999999") { message ->
-                            Log.d("service2", "1차 연결 : " + "연결된 워치 구독 중 -> " + JSONObject(message.payload).toString())
-
-                            val messageData = JSONObject(message.payload).getString("data")
-
-                            val inactiveWatchIds: List<WatchItem> = watchList.filter { !messageData.contains(it.watchId.toString()) }
-                            Log.d("inactiveWatchIds",inactiveWatchIds.toString())
-
-
-                            if (messageData.equals("[]")) {
-                                // 메시지 데이터가 비어 있으면, 데이터베이스의 모든 WatchItemEntity의 isConnected를 false로 설정
-                                val watchItemEntities = watchList.map {
-                                    WatchItemEntity(
-                                        watchId = it.watchId.toString(),
-                                        patientName = it.name,
-                                        patientRoom = it.host,
-                                        isConnected = false,
-                                        measuredDate = System.currentTimeMillis(),
-                                        modelName = "Abcdefghd"
-                                    )
-                                }
-                                db.watchItemDao().insertAll(watchItemEntities)
-                            } else {
-                                // 메시지 데이터가 비어 있지 않은 경우, 정상적으로 처리
-                                val watchItemEntities = watchList.map {
-                                    WatchItemEntity(
-                                        watchId = it.watchId.toString(),
-                                        patientName = it.name,
-                                        patientRoom = it.host,
-                                        isConnected = it.watchId.toString() in messageData,
-                                        measuredDate = System.currentTimeMillis(),
-                                        modelName = "Abcdefghd"
-                                    )
-                                }
-
-                                val inactiveWatchItemEntities = inactiveWatchIds.map {
-                                    WatchItemEntity(
-                                        watchId = it.watchId.toString(),
-                                        patientName = it.name,
-                                        patientRoom = it.host,
-                                        isConnected = false,
-                                        measuredDate = System.currentTimeMillis(),
-                                        modelName = "Abcdefghd"
-                                    )
-                                }
-                                val connectedWatchItemEntities = watchItemEntities.filter { it.isConnected }
-                                db.watchItemDao().insertAll(connectedWatchItemEntities)
-                                db.watchItemDao().insertAll(inactiveWatchItemEntities)
-                            }
-
-
-
-
-                            Log.d("service2", "1차 연결 : " + "연결된 워치 구독 완료")
-                            deferred.complete(Unit)
-                        }
-
-//                        deferred.await()
-
-                        Log.d("service2", "2차 연결 : " + "센서 데이터 구독 시작")
-
-                        val destinationList = watchIds.map { watchId ->
-                            "/queue/sensor/$watchId"
-                        }
-
-                        destinationList.forEach { destination ->
-                            client.subscribe(destination) { message ->
-                                Log.d("service2", message.payload)
-                                when (JSONObject(message.payload).getString("messageType")) {
-                                    "ACCELEROMETER" -> {
-                                        Log.d("service2", JSONObject(message.payload).getJSONObject("data").getDouble("accX").toString())
-                                        Log.d("service2", "ACCELEROMETER ok")
-                                        db.accXDao().insertData(AccXEntity(watchId = destination.split("/").lastOrNull() ?: "0",value = (JSONObject(message.payload).getJSONObject("data").getDouble("accX")).toFloat(), timeStamp = (JSONObject(message.payload).getJSONObject("data").getString("timeStamp"))))
-                                        db.accYDao().insertData(AccYEntity(watchId = destination.split("/").lastOrNull() ?: "0",value = (JSONObject(message.payload).getJSONObject("data").getDouble("accY")).toFloat(), timeStamp = (JSONObject(message.payload).getJSONObject("data").getString("timeStamp"))))
-                                        db.accZDao().insertData(AccZEntity(watchId = destination.split("/").lastOrNull() ?: "0",value = (JSONObject(message.payload).getJSONObject("data").getDouble("accZ")).toFloat(), timeStamp = (JSONObject(message.payload).getJSONObject("data").getString("timeStamp"))))
-
-                                        JSONObject(message.payload).getJSONObject("data").getDouble("accX")
-                                        JSONObject(message.payload).getJSONObject("data").getDouble("accY")
-                                        JSONObject(message.payload).getJSONObject("data").getDouble("accZ")
-                                    }
-                                    "HEART_RATE" -> {
-                                        Log.d("service2", "HEART_RATE ok")
-                                        db.heartRateDao().insertData(HeartRateEntity(watchId = destination.split("/").lastOrNull() ?: "0",value = JSONObject(message.payload).getJSONObject("data").getInt("value"), timeStamp = (JSONObject(message.payload).getJSONObject("data").getString("timeStamp"))))
-                                    }
-                                    "POSITION" -> {
-                                        val dataObject = JSONObject(message.payload).getJSONObject("data")
-                                        val watchId = dataObject.optString("watchId")
-                                        val position = dataObject.optString("position")
-                                        val name = dataObject.optString("watchName")
-
-                                        Intent().also { intent ->
-                                            intent.action = "com.example.asan_service.POSITION_UPDATE"
-                                            intent.putExtra("watchId",watchId)
-                                            intent.putExtra("position", position)
-                                            intent.putExtra("watchName",name)
-                                            LocalBroadcastManager.getInstance(this@MyWebSocketService).sendBroadcast(intent)
-                                        }
-                                    }
-                                    else -> {
-                                        Log.d("service2", "POSITION ok")
-                                    }
-                                }
-                            }
-                        }                    //
-
-                    }
-                } else {
-                    //
+                    subscribeToWatchListUpdates(watchList)
+                    subscribeToSensorData(watchIds)
                 }
             } catch (e: Exception) {
-                //
+                Log.e("service2", "Error during onStartCommand", e)
             }
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+    private fun CoroutineScope.subscribeToWatchListUpdates(watchList: MutableList<WatchItem>) {
+        launch {
+            client.subscribe("/queue/sensor/9999999") { message ->
+                val payload = JSONObject(message.payload)
+                Log.d("service2", "1차 연결 : 연결된 워치 구독 중 -> $payload")
+
+                when (payload.getString("messageType")) {
+                    "WATCH_LIST" -> handleWatchListMessage(payload.getString("data"), watchList)
+                    "NEW_WATCH" -> handleNewListMessage(payload.getString("data"))
+                    else -> Log.d("service2", "POSITION ok")
+                }
+            }
+        }
+    }
+
+    private fun handleWatchListMessage(data: String, watchList: MutableList<WatchItem>) {
+        Log.e("messageData", data)
+        Log.e("watchList after deleting "  , watchList.toString())
+        val inactiveWatchIds = watchList.filter { !data.contains(it.watchId.toString()) }
+        Log.d("inactiveWatchIds", inactiveWatchIds.toString())
+
+        val watchItemEntities = if (data == "[]") {
+            watchList.map {
+                WatchItemEntity(it.watchId.toString(), it.name, it.host, false, System.currentTimeMillis(), "Abcdefghd")
+            }
+        } else {
+            val activeWatchItems = watchList.map {
+                WatchItemEntity(it.watchId.toString(), it.name, it.host, true, System.currentTimeMillis(), "Abcdefghd")
+            }
+            val inactiveWatchItems = inactiveWatchIds.map {
+                WatchItemEntity(it.watchId.toString(), it.name, it.host, false, System.currentTimeMillis(), "Abcdefghd")
+            }
+            activeWatchItems + inactiveWatchItems
+        }
+
+        Log.e("watchItemEntities", watchItemEntities.toString())
+        db.watchItemDao().insertAll(watchItemEntities)
+    }
+
+    private fun handleNewListMessage(data: String) {
+        Log.d("service2", "NEW_LIST message received with data: $data")
+        val watchItemEntity = WatchItemEntity(
+            watchId = data,
+            patientName = "지정되지않음",
+            patientRoom = "지정되지않음",
+            isConnected = true,
+            measuredDate = System.currentTimeMillis(),
+            modelName = "Abcdefghd"
+        )
+
+        try {
+            db.watchItemDao().insert(watchItemEntity)
+            Log.d("service2", "New watch item inserted: $watchItemEntity")
+        } catch (e: Exception) {
+            Log.e("service2", "Error inserting new watch item", e)
+        }
+    }
+
+    private fun CoroutineScope.subscribeToSensorData(watchIds: Set<String>) {
+        launch {
+            Log.d("service2", "2차 연결 : 센서 데이터 구독 시작")
+            val destinations = watchIds.map { "/queue/sensor/$it" }
+
+            destinations.forEach { destination ->
+                client.subscribe(destination) { message ->
+                    val payload = JSONObject(message.payload)
+                    Log.d("service2", payload.toString())
+
+                    when (payload.getString("messageType")) {
+                        "ACCELEROMETER" -> handleAccelerometerMessage(payload, destination)
+                        "HEART_RATE" -> handleHeartRateMessage(payload, destination)
+                        "POSITION" -> handlePositionMessage(payload)
+                        else -> Log.d("service2", "Unknown message type")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleAccelerometerMessage(payload: JSONObject, destination: String) {
+        val data = payload.getJSONObject("data")
+        val watchId = destination.split("/").lastOrNull() ?: "0"
+        val accX = data.getDouble("accX").toFloat()
+        val accY = data.getDouble("accY").toFloat()
+        val accZ = data.getDouble("accZ").toFloat()
+        val timeStamp = data.getString("timeStamp")
+
+        db.accXDao().insertData(AccXEntity(watchId = watchId,value = accX, timeStamp =  timeStamp))
+        db.accYDao().insertData(AccYEntity(watchId = watchId,value = accY, timeStamp =  timeStamp))
+        db.accZDao().insertData(AccZEntity(watchId = watchId,value = accZ, timeStamp =  timeStamp))
+        Log.d("service2", "ACCELEROMETER ok")
+    }
+
+    private fun handleHeartRateMessage(payload: JSONObject, destination: String) {
+        val data = payload.getJSONObject("data")
+        val watchId = destination.split("/").lastOrNull() ?: "0"
+        val value = data.getInt("value")
+        val timeStamp = data.getString("timeStamp")
+
+        db.heartRateDao().insertData(HeartRateEntity(watchId = watchId, value = value, timeStamp = timeStamp))
+        Log.d("service2", "HEART_RATE ok")
+    }
+
+    private fun handlePositionMessage(payload: JSONObject) {
+        val dataObject = payload.getJSONObject("data")
+        val watchId = dataObject.optString("watchId")
+        val position = dataObject.optString("position")
+        val name = dataObject.optString("watchName")
+        if(position.isNotEmpty() && !position.equals("null")){
+            PositionRepository.updatePosition(watchId, position, name)
+        }
+
+        timers[watchId]?.cancel()
+
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            delay(20000)
+            PositionRepository.removePosition(watchId)
+            timers.remove(watchId)
+        }
+
+        timers[watchId] = job
+    }
+
 
     private fun createStompClient(): StompClient {
         val okHttpClient = OkHttpClient.Builder()
