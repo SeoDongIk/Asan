6491464e6dd1186b6242dbfd7b3bbbe8ab701c67
Jedo0224 <qkrwotjr0@gmail.com>
2024-07-09 09:01:14 +0900

    [feat] 소켓 센서 서버와 위치 서버로 분할하여 연결.

--- app/src/main/java/com/example/asan_service/util/StaticResource.kt
+++ app/src/main/java/com/example/asan_service/util/StaticResource.kt
@@ -5,13 +5,22 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object StaticResource {
-//    private const val ServerURL = "210.102.178.186"
-    private const val ServerURL = "192.168.37.213"
+    private const val ServerURL = "172.25.53.139"
+//        private const val ServerURL = "192.168.37.213"
     private const val port = "8080"
+    private const val portForPosition = "8083"
 
-    val apiService: ApiService by lazy {
+    val apiServiceForSensor: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(getHttpURL())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+
+    val apiServiceForPosition: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(getHttpURLForPosition())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         retrofit.create(ApiService::class.java)
@@ -21,11 +30,29 @@ object StaticResource {
         return if (port.isEmpty()) "http://$ServerURL/" else "http://$ServerURL:$port/"
     }
 
-    fun getHttpUrlWithoutSlash(): String {
-        return if (port.isEmpty()) "http://$ServerURL" else "http://$ServerURL:$port"
+    fun getHttpUrlForPosition(): String {
+        return if (port.isEmpty()) "http://$ServerURL/" else "http://$ServerURL:$port/"
+    }
+
+    fun getHttpUrlForPositionWithoutSlash(): String {
+        return if (port.isEmpty()) "http://$ServerURL/" else "http://$ServerURL:$portForPosition"
     }
 
     fun getWsURL(): String {
         return if (port.isEmpty()) "ws://$ServerURL/" else "ws://$ServerURL:$port/ws"
     }
+
+    fun getWsUrlForPosition(): String {
+        return if (port.isEmpty()) "ws://$ServerURL/" else "ws://$ServerURL:$portForPosition/ws"
+    }
+
+    fun getHttpURLForPosition(): String {
+        return if (port.isEmpty()) "http://$ServerURL/" else "http://$ServerURL:$portForPosition/"
+    }
+
+    fun getHttpUrlWithoutSlash(): String {
+        return if (port.isEmpty()) "http://$ServerURL" else "http://$ServerURL:$port"
+    }
+
+
 }
